ff3df2568b123b3add850d12f71516a7d146903e
rakesh2026cse <rakeshkumarsahoo2026cse@gmail.com>
2025-04-14 11:45:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,11 @@
-# config.py
 
-# GPIO pin setup
-SOIL_SENSOR_PIN = 17  # For digital soil sensor
-TEMP_SENSOR_PIN = 4   # DHT11 connected to GPIO4
 
-# Alert thresholds
-MOISTURE_DRY = 0      # Digital LOW = Dry
-TEMP_THRESHOLD = 35   # °C threshold for temperature warning
+SOIL_SENSOR_PIN = 17  
+TEMP_SENSOR_PIN = 4   
 
-# Time interval between sensor reads (in seconds)
+MOISTURE_DRY = 0      
+TEMP_THRESHOLD = 35   
+
 SENSOR_INTERVAL = 15
 
 DB_NAME = "database.db"
